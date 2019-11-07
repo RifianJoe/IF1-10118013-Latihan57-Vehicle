97301ece6b1ee27b6f20d31ba824658815622399
Rifian Joe Premilenio <55788355+RifianJoe@users.noreply.github.com>
2019-11-07 21:58:03 +0800

    Add files via upload

---
+++ IF1-10118013-Latihan57-Vehicle/src/if1/pkg10118013/latihan57/vehicle/IF110118013Latihan57Vehicle.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118013.latihan57.vehicle;
+
+/**
+ *
+ * @author Rifian
+ * Nama : Rifian Joe Premilenio
+ * Kelas : IF-01
+ * NIM : 10118013
+ */
+public class IF110118013Latihan57Vehicle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Bicycle sepeda = new Bicycle();
+        System.out.println();
+        Skateboard skateboard = new Skateboard();
+    }
+    
+}
